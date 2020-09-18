188c04651f979a49d41b68a8e8bc1ea4ad01c133
jamiemott <49801217+jamiemott@users.noreply.github.com>
2020-09-18 08:22:07 -0600

    A little clean up

--- smshell.c
+++ smshell.c
@@ -1,11 +1,6 @@
 /* Small Shell
- * Name: Jamie Mott
+ * Author: Jamie Mott
  * Date: February 25, 2020
- * Description: This program creates a shell.  User input is entered and parsed into commands. 
- *			The shell recognizes three built-in commands: cd, status, and exit.  All other commands are
- *			forked and handled either in the foreground or background as designated. The user can change
- *			whether processes can run in the background. A list of active background processes is kept and
- *			all child processes are killed before the shell exits.
  */
 
 
